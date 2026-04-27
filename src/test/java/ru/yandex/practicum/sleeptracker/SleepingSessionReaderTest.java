c7eaad98b96ee1dd63a41a907af91483c4ad43e0
edkalaev <kalaeved@gmail.com>
2026-04-28 00:00:22 +0300

    поправить ошибку автотеста SleepingSessionReaderTest

--- src/test/java/ru/yandex/practicum/sleeptracker/SleepingSessionReaderTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/SleepingSessionReaderTest.java
@@ -2,11 +2,9 @@ package ru.yandex.practicum.sleeptracker;
 
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SleepingSessionReaderTest {
@@ -14,27 +12,22 @@ class SleepingSessionReaderTest {
     private final SleepingSessionReader reader = new SleepingSessionReader();
 
     @Test
-    void shouldReadAllSessionsFromResourceFile() throws IOException, URISyntaxException {
+    void shouldReadAllSessionsFromResourceFile() throws IOException {
         List<SleepingSession> sessions = reader.readFromFile(getResourcePath());
-
         assertEquals(13, sessions.size());
     }
 
     @Test
-    void shouldReadFirstSessionCorrectlyFromResourceFile() throws IOException, URISyntaxException {
+    void shouldReadFirstSessionCorrectlyFromResourceFile() throws IOException {
         List<SleepingSession> sessions = reader.readFromFile(getResourcePath());
-        SleepingSession firstSession = sessions.getFirst();
+        SleepingSession firstSession = sessions.get(0);
 
         assertEquals(LocalDateTime.of(2025, 10, 1, 23, 15), firstSession.getSleepTime());
         assertEquals(LocalDateTime.of(2025, 10, 2, 7, 30), firstSession.getWakeTime());
         assertEquals(SleepQuality.GOOD, firstSession.getQuality());
     }
 
-    private String getResourcePath() throws URISyntaxException {
-        return Path.of(
-                Objects.requireNonNull(
-                        getClass().getClassLoader().getResource("sleep_log.txt")
-                ).toURI()
-        ).toString();
+    private String getResourcePath() {
+        return Path.of("src", "test", "resources", "sleep_log.txt").toString();
     }
 }
